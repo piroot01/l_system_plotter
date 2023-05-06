8621d1bdd406015875c8d720d9f560f5e3abddf1
Tomáš Macháček <piroot01@gmail.com>
2023-05-06 11:18:55 +0200

    Time.hpp: constructors

--- include/Time.hpp
+++ include/Time.hpp
@@ -12,11 +12,15 @@ private:
 
 public:
     CurrentTime(const std::string& format = "%d-%m-%Y %H:%M:%S") : m_timeFormat(format) {}
+    ~CurrentTime() = default;
 
     void SetFormat(const std::string& format);
     std::string GetCurrentTime() const;
 
+    CurrentTime(const CurrentTime&) = delete;
+    CurrentTime(CurrentTime&&) = delete;
     CurrentTime& operator=(const CurrentTime&) = delete;
+    CurrentTime& operator=(CurrentTime&&) = delete;
 };
 
 #endif
